eb69202aacee60d478e5e623017eb7e24e5b728a
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-02 11:13:33 -0700

    adds various routes surrounding sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'pages#home'
+  resources :users, only: [:show, :new, :create, :edit, :update]
   get '/about', to: 'pages#about'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
