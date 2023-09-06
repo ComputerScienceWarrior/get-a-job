9ee82e20a860a3cc756f1c672e82556d8e4729ee
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-06 09:43:01 -0700

    Adds enum to Portfolio model for different types of Portfolio

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,9 @@
 class Template < ApplicationRecord
     has_one :portfolio
+
+    enum type: {
+        beginner: 'beginner',
+        intermediate: 'intermediate',
+        advanced: 'advanced'
+    }
 end
