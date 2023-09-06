a8147416140bb04ab3eb79fcf87698b593008c63
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-06 04:32:11 -0700

    adds validation #validate_portfolio_limit to prevent more than 3 portfolios per user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
+    has_many :portfolios, limit: 3
     has_secure_password
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
+
+    validate :validate_portfolio_limit
+
+    private
+
+    def validate_portfolio_limit
+        if portfolios.count >= 3
+            errors.add(:base, "You can only have a maximum of 3 portfolios.")
+        end
+    end
 end
