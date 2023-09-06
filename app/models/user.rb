9fd9d67712676d8f5d8ab648e0d8a5e70ac369c6
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-06 10:16:38 -0700

    hotfix: fix bug with has_many :portfolios  missing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :portfolios
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
 
