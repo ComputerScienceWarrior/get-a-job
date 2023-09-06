0bb55b8e40b3dfa3b646999785164ffceec712bd
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-06 09:54:05 -0700

    removes accidental code leave-in from model file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-    has_many :portfolios, limit: 3
     has_secure_password
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
