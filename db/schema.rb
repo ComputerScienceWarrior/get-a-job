1126fddc3ce8546cd8fcc76c35ac963086850c36
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-06 10:41:11 -0700

    adds 'about_me' attr to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 2023_09_06_173358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 4) do
     t.text "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "about_me", default: ""
   end
 
   add_foreign_key "portfolios", "templates"
