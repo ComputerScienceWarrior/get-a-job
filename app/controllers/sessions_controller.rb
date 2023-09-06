23c093c22a0444f2d7f411c015c6a65ae9f086ba
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-05 18:41:13 -0700

    adds flash alerts to login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      redirect_to user_path(user), notice: 'Welcome Back!'
     else
-      flash.now[:alert] = 'Invalid email/password combination'
+      flash.now[:alert] = 'Invalid email/password combination!'
       render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'Logged out successfully'
+    redirect_to root_path, notice: 'Logged out successfully!'
   end
 end
