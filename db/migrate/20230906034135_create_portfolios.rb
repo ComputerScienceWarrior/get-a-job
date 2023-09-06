7e7a68358896af651a289243f44eb8c1334e972a
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-06 04:36:52 -0700

    creates initial migration for Portfolio Model

---
+++ db/migrate/20230906034135_create_portfolios.rb
@@ -0,0 +1,9 @@
+class CreatePortfolios < ActiveRecord::Migration[6.1]
+  def change
+    create_table :portfolios do |t|
+      t.text :title
+      t.timestamps
+    end
+    add_reference :portfolios, :template, foreign_key: true
+  end
+end
