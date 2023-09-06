2b5f372f5c70a026925e3b184765709ff80d98a9
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-06 04:38:05 -0700

    adds :description as an attr

--- db/migrate/20230906034135_create_portfolios.rb
+++ db/migrate/20230906034135_create_portfolios.rb
@@ -2,6 +2,7 @@ class CreatePortfolios < ActiveRecord::Migration[6.1]
   def change
     create_table :portfolios do |t|
       t.text :title
+      t.text :description
       t.timestamps
     end
     add_reference :portfolios, :template, foreign_key: true
