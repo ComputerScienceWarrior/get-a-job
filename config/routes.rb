8d6e039f6708b32631d95c7c701735e507dcc165
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-06 17:20:51 -0700

    adds routing for portfolios and user_portfolios

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show, :new, :create, :edit, :update] do
+    resources :portfolios, only: [:index]
+  end
+  resources :portfolios, only: [:index]
+  
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  root 'pages#home'
-  resources :users, only: [:show, :new, :create, :edit, :update]
   get '/about', to: 'pages#about'
   get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  post '/signup', to: 'users#create'
+
+  root 'pages#home'
 end
