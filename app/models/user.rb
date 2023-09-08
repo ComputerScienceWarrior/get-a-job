d1a1cf1e304baf56ef9eba4055b3d6775c214950
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-08 12:06:35 -0700

    removes bust method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,7 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
 
-    validate :validate_portfolio_limit
-
     def fullname
-        firstname + ' ' + lastname
+      "#{firstname} #{lastname}"
     end
-
-    private
-
-    def validate_portfolio_limit
-        if portfolios.count >= 3
-            errors.add(:base, "You can only have a maximum of 3 portfolios.")
-        end
-    end
-end
+  end
