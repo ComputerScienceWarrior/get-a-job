c80285532468497a338f4b41a0fed6b6590a53f5
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-09-07 19:19:57 -0700

    fixes routing structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :portfolios
-  end
   resources :portfolios, only: [:index]
-  
+
+  resources :users do
+    resources :portfolios, controller: 'users/portfolios'
+  end
+
+
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get '/about', to: 'pages#about'
-  get '/signup', to: 'users#new'
+  post '/login', to: 'sessions#create'
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
-  post '/login', to: 'sessions#create'
+
+  get '/about', to: 'pages#about'
+
+  get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
   root 'pages#home'
